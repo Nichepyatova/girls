c98fba4cd52d57f48ea93054a82ba6d495b1845e
Nichepaytova <wunata95@gmail.com>
2023-01-26 23:16:34 +0600

    modifited templates

--- girls/app/views.py
+++ girls/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 
 def post_list(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
